5928493195617c5df6322e4c480062ed8a7add33
Nerhaus-Playing <Nerdhaus.playing@gmail.com>
2019-09-07 18:40:13 +0200

    Initial Setup

---
+++ src/exampleLib.cpp
@@ -0,0 +1,9 @@
+#include "exampleLib.h"
+#include <iostream>
+
+using namespace std;
+
+void exampleFunction()
+{
+	cout << "Example" << endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#incldue "exampleLib.h"
+
+using namespace std;
+void main()
+{
+	cout << "Hello World!" << endl;
+	exampleFunction();
+}
